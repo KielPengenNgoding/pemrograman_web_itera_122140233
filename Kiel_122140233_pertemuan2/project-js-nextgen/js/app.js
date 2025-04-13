8405485c369c1d100ae5f9d415a7b17666304f2f
KielPengenNgoding <145269314+KielPengenNgoding@users.noreply.github.com>
2025-04-13 23:29:41 +0700

    Delete Kiel_122140233_pertemuan2 directory

--- Kiel_122140233_pertemuan2/project-js-nextgen/js/app.js
+++
@@ -1,7 +0,0 @@
-import { TaskManager } from './modules/data.js';
-import { ScheduleManager } from './modules/data.js';
-import { WeatherService } from './modules/data.js';
-import { displayDateTime, showElement, hideElement } from './modules/utils.js';
-import { initApp } from './main.js';
-
-document.addEventListener('DOMContentLoaded', initApp);
